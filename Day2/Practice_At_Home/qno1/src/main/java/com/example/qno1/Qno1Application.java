afdea321c13dd48f8e1e2291dcc6ad76d487ce42
Sriram484 <116054870+Sriram484@users.noreply.github.com>
2024-04-13 09:47:40 +0530

    commit 2

---
+++ Day2/Practice_At_Home/qno1/src/main/java/com/example/qno1/Qno1Application.java
@@ -0,0 +1,13 @@
+package com.example.qno1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qno1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qno1Application.class, args);
+	}
+
+}
---
+++ Day2/Practice_At_Home/qno1/src/test/java/com/example/qno1/Qno1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qno1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qno1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
