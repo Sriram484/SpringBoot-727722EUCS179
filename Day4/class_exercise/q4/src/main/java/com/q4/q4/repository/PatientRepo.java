31e8edb8d012722ff65cf57344fe22588d8bf318
Sriram484 <116054870+Sriram484@users.noreply.github.com>
2024-04-13 09:48:56 +0530

    DAY 4 & DAY 5 DONE

--- Day4/class_exercise/q4/src/main/java/com/q4/q4/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.q4.q4.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.q4.q4.model.Patient;
-
-public interface PatientRepo extends JpaRepository <Patient,Integer>{
-
-}
